e4be64daa715370a742ab22cbcac53f3f977c48c
Sofia Buitrago <sofia.buitrago@yopmail.com>
2024-06-20 12:54:21 -0500

    Simple Calculator with OOP

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Calculator {
+
+        public Calculator (){
+
+        }
+
+    public static int Addition (int a, int b){
+        int res = a + b;
+        return res;
+
+    }
+
+    public static int Subtraction (int a, int b){
+        int res = a - b;
+        return res;
+
+    }
+
+    public static int Multiplication (int a, int b){
+        int res = a * b;
+        return res;
+
+    }
+
+    public static int Division (int a, int b){
+        int res = a / b;
+        return res;
+
+    }
+
+}
